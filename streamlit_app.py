ff2f5cc78d99f8941af57c874950b29da7fc1a99
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-09-29 12:28:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,55 @@
+import openai
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title("Customer simulator")
 
-st.write('Hello world!')
+openai.api_key = st.secrets["openai"]
+
+# Create an empty string to store the conversation
+conversation = ""
+
+with st.form("input form"):
+    st.write("<h3>Enter the customer personae ✨</h3>", unsafe_allow_html=True)
+    customer_persona = st.text_input("Enter the customer persona:")
+
+    if st.form_submit_button("Initiate discussion"):
+        if customer_persona is not None:
+            messages = [
+                {"role": "system", "content": customer_persona},
+            ]
+            
+        if "openai_model" not in st.session_state:
+            st.session_state["openai_model"] = "gpt-3.5-turbo"
+        
+        if "messages" not in st.session_state:
+            st.session_state.messages = messages
+        
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        
+        # Create the conversation by including the customer persona as part of the initial message
+        conversation = [{"role": "system", "content": customer_persona}]
+        conversation.extend(st.session_state.messages)  # Include other conversation messages
+        conversation.append({"role": "user", "content": prompt})  # Include user input
+        
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=conversation,
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+
+            
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
