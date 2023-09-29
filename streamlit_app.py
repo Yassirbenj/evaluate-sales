0b2dec5cd90aa65b4355b6c44ecf1377ff55ae01
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-09-29 12:43:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,51 +5,40 @@ st.title("Customer simulator")
 
 openai.api_key = st.secrets["openai"]
 
-# Create an empty string to store the conversation
-conversation = ""
+#context of the discussion
+context= "Evaluate this discussion between a sales person and a customer. evaluate if a sales person arguments for a sell are convincing or not as regard different factors"
+messages = [{"role": "system", "content": context}]
 
 with st.form("input form"):
-    st.write("<h3>Enter the customer personae ✨</h3>", unsafe_allow_html=True)
-    customer_persona = st.text_input("Enter the customer persona:")
+    st.write("<h3>Enter the discussion between sales and customer ✨</h3>", unsafe_allow_html=True)
+    discussion = st.text_input("Enter the discussion text:")
 
-    if st.form_submit_button("Initiate discussion"):
-        if customer_persona is not None:
-            messages = [
-                {"role": "system", "content": customer_persona},
-            ]
+    if st.form_submit_button("Evaluate"):
+        if discussion is not None:
+            if "openai_model" not in st.session_state:
+                st.session_state["openai_model"] = "gpt-3.5-turbo"
             
-        if "openai_model" not in st.session_state:
-            st.session_state["openai_model"] = "gpt-3.5-turbo"
-        
-        if "messages" not in st.session_state:
-            st.session_state.messages = messages
-        
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-        
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+            if "messages" not in st.session_state:
+                st.session_state.messages = messages
+            
+            for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
+            st.session_state.messages.append({"role": "user", "content": discussion})
+
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
         
-        # Create the conversation by including the customer persona as part of the initial message
-        conversation = [{"role": "system", "content": customer_persona}]
-        conversation.extend(st.session_state.messages)  # Include other conversation messages
-        conversation.append({"role": "user", "content": prompt})  # Include user input
-        
-        for response in openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=conversation,
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
+            for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=conversation,
+                stream=True,
+            ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
 
             
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+            message_placeholder.markdown(full_response)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
